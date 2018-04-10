e49fb3b466cff017b6dd4387e94d24664082ac4d
estrattonbailey <ericstrattonbailey@gmail.com>
2018-04-09 21:25:11 -0400

    .

--- dist/tinkerbell.js
+++
@@ -1,2 +0,0 @@
-module.exports=function(n,t,r,e){var o=t-n;return function(u,a){var i,c,f,m=Date.now();return c=function c(d){return requestAnimationFrame(function(d){f=Math.round(e(Date.now()-m,n,o,r)),(t<n?f>t:f<t)&&!i?(c(),u(f)):a&&a()})}(),function(){cancelAnimationFrame(c),i=!0}}};
-//# sourceMappingURL=tinkerbell.js.map
