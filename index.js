b6c90a68459010a26df77d59b925fdb022978c56
estrattonbailey <ericstrattonbailey@gmail.com>
2018-04-12 16:59:49 -0400

    Fix boomerang

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
 export default function tinkerbell (b, e, d, ease) {
+  b = Math.round(b)
+  e = Math.round(e)
   const c = e - b
 
   return function tween (cb, done) {
-    let ts = Date.now()
+    let ts
     let stopped
     let frame
+    let last
     let next
 
     frame = (function tick (last) {
       return requestAnimationFrame(t => {
-        next = Math.round(ease(Date.now() - ts, b, c, d))
-        if ((e < b ? next > e : next < e) && !stopped) {
+        if (!ts) ts = t
+        last = next || b
+        next = Math.round(ease(t - ts, b, c, d))
+        if ((e > b ? next < e && last <= e : next > e && last >= e) && !stopped) {
           tick(next)
           cb(next)
         } else {
           done && done()
+          stopped = true
+          cancelAnimationFrame(frame)
         }
       })
     })(b)
