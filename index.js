82181f33ac5a911f05a1fd00563153402e24ec49
estrattonbailey <ericstrattonbailey@gmail.com>
2018-04-12 20:06:29 -0400

    Respect duration

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 export default function tinkerbell (b, e, d, ease) {
-  b = Math.round(b)
-  e = Math.round(e)
-  const c = e - b
-
   return function tween (cb, done) {
+  	let te
     let ts
-    let stopped
     let frame
     let last
     let next
@@ -13,22 +9,20 @@ export default function tinkerbell (b, e, d, ease) {
     frame = (function tick (last) {
       return requestAnimationFrame(t => {
         if (!ts) ts = t
-        last = next || b
-        next = Math.round(ease(t - ts, b, c, d))
-        if ((e > b ? next < e && last <= e : next > e && last >= e) && !stopped) {
-          tick(next)
+        te = t - ts
+        next = Math.round(ease(te, b, e - b, d))
+        if ((e > b ? next < e && last <= e : next > e && last >= e) && te <= d) {
+          frame = tick(next)
           cb(next)
         } else {
+        	cb(e)
           done && done()
-          stopped = true
-          cancelAnimationFrame(frame)
         }
       })
     })(b)
 
     return function stop () {
       cancelAnimationFrame(frame)
-      stopped = true
     }
   }
 }
